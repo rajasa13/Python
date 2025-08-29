6a77524da94eed9efd6473fa984782faf8310a0c
rajasa13 <mcrajasa@gmail.com>
2025-08-29 16:05:19 +0700

    new function: add item to inventory

--- Inventory/main.py
+++ Inventory/main.py
@@ -14,3 +14,18 @@ def load_inventory():
 def save_inventory(inventory):
     with open(INVENTORY_FILE, 'w') as file:
         json.dump(inventory, file, indent=4)
+
+# Add a new item to the inventory
+def add_item(inventory):
+    item_id = input("Enter item ID: ")
+    if item_id in inventory:
+        print("Item ID already exists!")
+        return
+    name = input("Enter item name: ")
+    unit = input("Enter item unit (e.g., kg, pcs): ")
+    quantity = int(input("Enter item quantity: "))
+    inventory[item_id] = {'name': name, 
+                          'unit': unit, 
+                          'quantity': quantity}
+    print("Item added successfully!")
+
