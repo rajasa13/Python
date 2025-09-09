345d519ddc065ca3da6696e9b95438938b533f01
rajasa13 <mcrajasa@gmail.com>
2025-09-09 21:13:34 +0700

    add: encapsulation concept

---
+++ encapsulation.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name # public attribute
+        self._age = age # "protected" by convention (single underscore)
+
+    def introduce(self):
+        return f"My name is {self.name} and I am {self.age} years old."
+    
+p1 = Person("Alice", 30)
+print(p1.introduce())
