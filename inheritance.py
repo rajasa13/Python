0ba3c860a10e8a27966befbc1e00468f0d63e45f
rajasa13 <mcrajasa@gmail.com>
2025-09-09 22:04:21 +0700

    bug fix: inheritance concept

--- inheritance.py
+++ inheritance.py
@@ -1,6 +1,6 @@
 class Vehicle:
     def __init__(self, brand):
-        self.band = brand
+        self.brand = brand
 
     def drive(self):
         return "Moving fordward"
@@ -12,4 +12,4 @@ class Car(Vehicle): # inherits from vehicle
 my_car = Car("Toyota")
 print(my_car.brand)  # Inherited from Vehicle
 print(my_car.drive()) # Inherited method
-print(my_car.hon()) # Car's own method
+print(my_car.honk()) # Car's own method
